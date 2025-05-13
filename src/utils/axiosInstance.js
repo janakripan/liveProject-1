ed216bc6c0e5b7e846a3cb43b26760fc5b52ef9c
janakripan <janakripanjkc@gmail.com>
2025-05-13 11:22:14 +0530

    changed axios instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'accept': '/',
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true, 
 });
 
 export default axiosInstance;
