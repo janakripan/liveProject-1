8ba6bb28910e510defc6a94f036215e195e519da
janakripan <janakripanjkc@gmail.com>
2025-05-15 11:34:00 +0530

    added pwa option

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import {QueryClient , QueryClientProvider} from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
+import { Workbox } from 'workbox-window';
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js');
+  wb.addEventListener('activated', (event) => {
+    console.log('Service worker activated:', event);
+  });
+  wb.register();
+}
 
 const queryClient = new QueryClient()
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,71 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from "rollup-plugin-visualizer";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(),visualizer({ open: true })],
+  plugins: [
+    tailwindcss(),
+    react(),
+    visualizer({ open: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Zudoku",
+        short_name: "VitePWA",
+        description: "Zudoku api documentation",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "zudoku-logo-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "zudoku-logo-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+       workbox: {
+    runtimeCaching: [
+      {
+        urlPattern:  /^https:\/\/alnibraz-bufzaeheasasbbgf\.southindia-01\.azurewebsites\.net\/api\/.*/i, // replace with your actual API URL
+        handler: 'NetworkOnly', // 🛡️ Do not cache login/API requests
+        options: {
+          cacheName: 'api-no-cache',
+        },
+      },
+      {
+        // optionally cache static assets like images, fonts, etc.
+        urlPattern: /\.(?:png|jpg|jpeg|svg|gif|woff2?|eot|ttf|otf)$/i,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'assets-cache',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+          },
+        },
+      },
+    ],
+  },
+    }),
+  ],
   server: {
     historyApiFallback: true,
-  }
+  },
+   preview: {
+    port: 5173, // or any port you want
+  },
 });
